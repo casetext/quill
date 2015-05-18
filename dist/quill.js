e0a249cdf47e1f5cff311602e6afc2133c67961a
Tom G Varik <tom@casetext.com>
2015-05-18 11:02:16 -0700

    rebuild

--- dist/quill.js
+++ dist/quill.js
@@ -6515,9 +6515,11 @@ Format = (function() {
       exclude: ['h1', 'h2', 'bullet', 'list', 'blockquote'],
       tag: 'H3'
     },
-    opacity: {
-      style: 'opacity',
-      "default": '1'
+    invisible: {
+      "class": 'invisible'
+    },
+    dashedUnderline: {
+      "class": 'dashed-underline'
     },
     pdf: {
       type: Format.types.EMBED,
